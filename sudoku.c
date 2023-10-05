5305e1c1bf0f1e2f07a27c40a38ecaab1fb3ecdf
Replit user <>
2023-10-05 03:09:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -98,7 +98,7 @@ List* get_adj_nodes(Node* n){
       if(n->sudo[i][j]==0){
         for(int k=1;k<=9;k++){
           Node* copialist= copy(n);
-          copialist[i][j]=k;
+          copialist->sudo[i][j]=k;
           if(is_valid(copialist)){
             pushBack(list,copialist);
           }
